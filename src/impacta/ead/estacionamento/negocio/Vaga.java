d2200e77928da0f2b9eb8af7a366dcde62a8ec8b
André Carvalho <andre_sdc@hotmail.com>
2024-02-12 18:26:47 -0300

    update 12/02/24

--- src/impacta/ead/estacionamento/negocio/Vaga.java
+++ src/impacta/ead/estacionamento/negocio/Vaga.java
@@ -7,16 +7,44 @@ package impacta.ead.estacionamento.negocio;
  */
 public class Vaga {
 	public static int TOTAL_VAGAS = 100;
-	private int vagasOcupadas =0;
+	private static int vagasOcupadas =0;
 	
 	/*
 	 * Verificar a existencia de alguma vaga livre no estacionamento 
 	 * 
 	 * @return true se houver alguma vaga e false se estiver lotado
 	 */
-	public boolean temVagaLivre() {
+	public static boolean temVagaLivre() {
 		//TODO implementar este metodo
 		return false;
 	}
 	
+	/**
+	 * Buscar status atual das vagas do estacionamento
+	 */
+	public static void inicializarOcupadas() {
+		//TODO implementar este metodo
+	}
+	
+	/**
+	 * Retorna o numero de vagas ocupadas 
+	 * @return numero total de vagas ocupadas num determinado instante
+	 */
+	public static int ocupadas() {
+		return Vaga.vagasOcupadas;
+	}
+	
+	/**Retorna o numero de vagas livres
+	 * @return numero total de vagas livres num determinado instante
+	 */
+	public static int livres() {
+		return TOTAL_VAGAS - Vaga.vagasOcupadas;
+	}
+	
+	/**
+	 * Atualiza o numero de vagas ocupadas apos a entrada do veiculo
+	 */
+	public static void entrou() {
+		Vaga.vagasOcupadas ++;
+	}
 }
